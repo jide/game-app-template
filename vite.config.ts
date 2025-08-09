c5b0ae1043dd4c68b235bb1a1e80d94df7a38e20
jide <jide.fr@gmail.com>
2025-08-10 01:34:46 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    host: true,
+    allowedHosts: true,
+  },
   esbuild: {
     tsconfigRaw: {
       compilerOptions: {
